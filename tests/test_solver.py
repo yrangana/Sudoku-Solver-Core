c7de86cb75c665d05aec3a43da85e4df0db4831e
Yasiru <yrangana@gmail.com>
2024-12-31 15:37:51 +1100

    test cases for solver

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -54,3 +54,23 @@ def test_unsolvable_puzzle():
         [0, 0, 0, 0, 8, 0, 0, 7, 9],
     ]
     assert not solve_sudoku(unsolvable_puzzle)
+    
+def test_is_valid_grid():
+    valid_grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+    invalid_grid = [
+        [5, 3, 0, 0, 7],  # Not 9x9
+        [6, 0, 0, 1, 9, 5],
+        [0, 9, 8, 0],
+    ]
+    assert is_valid_grid(valid_grid)  # Should return True
+    assert not is_valid_grid(invalid_grid)  # Should return False
